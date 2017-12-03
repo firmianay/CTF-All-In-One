571cc031ebd5551ff6518fe34059472f930dd12a
firmianay <firmianay@gmail.com>
2017-12-03 22:11:11 +0800

    fix

--- src/writeup/6.2.3_re_codegate2017_angrybird/exp.py
+++ src/writeup/6.2.3_re_codegate2017_angrybird/exp.py
@@ -1,10 +1,10 @@
 import angr
 
 main  = 0x004007da
-find  = 0x00404fc1
+find  = 0x00404fda  # leave;ret
 avoid = 0x00400590  # puts@plt
 
-p = angr.Project('./angrybird_mod')
+p = angr.Project('./angrybird_org')
 init = p.factory.blank_state(addr=main)
 pg = p.factory.simgr(init, threads=4)
 ex = pg.explore(find=find, avoid=avoid)

a9e9b644b582fb2690f580e0348281576786edea
firmianay <firmianay@gmail.com>
2017-12-05 16:24:59 +0800

    finish 6.2.4

---
+++ src/writeup/6.2.4_re_csawctf2015_wyvern/exp_pin.py
@@ -0,0 +1,24 @@
+import os
+
+def get_count(flag):
+    cmd = "echo " + "\"" + flag + "\"" + " | ../../../pin -t obj-intel64/wyvern.so -o inscount.out -- ~/wyvern "
+    os.system(cmd)
+    with open("inscount.out") as f:
+        count = int(f.read().split(" ")[1])
+    return count
+
+charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-+*'"
+
+flag = list("A" * 28)
+count = 0
+for i in range(28):
+    for c in charset:
+        flag[i] = c
+        # print("".join(flag))
+        count = get_count("".join(flag))
+        # print(count)
+        if count == i+2:
+            break
+    if count == 29:
+        break;
+print("".join(flag))
